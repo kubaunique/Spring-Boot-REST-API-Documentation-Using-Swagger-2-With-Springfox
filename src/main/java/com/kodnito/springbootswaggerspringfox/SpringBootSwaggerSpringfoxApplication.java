afe1494316dc3e56c56f280b3af774717727d881
Hayri Cicek <hello@hayricicek.se>
2018-08-02 12:20:52 +0200

    Add files via upload

---
+++ src/main/java/com/kodnito/springbootswaggerspringfox/SpringBootSwaggerSpringfoxApplication.java
@@ -0,0 +1,12 @@
+package com.kodnito.springbootswaggerspringfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerSpringfoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwaggerSpringfoxApplication.class, args);
+	}
+}
